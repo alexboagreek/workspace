a1cd8adc4917db2798e0b9f55ac69903dad2ea1f
Alex <olivesargreek@gmail.com>
2023-08-29 13:40:30 +0300

    feat: getData() && init() первостепенные функции

--- js/script.js
+++ js/script.js
@@ -1,6 +1,39 @@
-const citySelect = document.querySelector('#city');
-console.log(citySelect);
-const cityChoices = new Choices(citySelect, {
-  searchEnabled: false,
-  itemSelectText: '',
-});
+const API_URL = "https://workspace-methed.vercel.app/";
+const LOCATION_URL = "api/locations";
+
+const getData = async (url, cbSuccess, cbError) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    cbSuccess(data);
+  } catch (error) {
+    cbError(error);
+  }
+};
+
+const init = () => {
+  const citySelect = document.querySelector("#city");
+  const cityChoices = new Choices(citySelect, {
+    itemSelectText: "",
+  });
+
+  getData(
+    `${API_URL}${LOCATION_URL}`,
+    (locationData) => {
+      const locations = locationData.map(location => ({
+        value: location,
+      }));
+      cityChoices.setChoices(
+        locations,
+        "value",
+        "label",
+        false,
+      );
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+};
+
+init();
